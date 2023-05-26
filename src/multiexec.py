702b9205197fa8260289de2c48ac39a8c21c6a5d
Rasmus Bahbah Nielsen <114926145+RasmusBahbah@users.noreply.github.com>
2023-05-26 11:51:32 +0200

    Changed Folder and added no double download feat.

---
+++ src/multiexec.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 13:34:21 2023
+
+@author: rabni
+"""
+
+import argparse
+import pandas as pd
+import sys 
+import carra2py
+import logging
+import glob
+import os
+import time
+from multiprocessing import set_start_method,get_context
+# try:
+#     set_start_method("spawn")
+# except:
+#     pass
+
+if sys.version_info < (3, 4):
+    raise "must use python 3.6 or greater"
+    
+base_f  = os.path.abspath('..')
+
+if not os.path.exists(base_f + os.sep + "logs"):
+        os.makedirs(base_f + os.sep + "logs")
+        
+log_f = base_f + os.sep + "logs"
+logging.basicConfig(
+        format='%(asctime)s [%(levelname)s] %(name)s - %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.FileHandler(f'{log_f}' + os.sep + f'multiexec_{time.strftime("%Y_%m_%d",time.localtime())}.log'),
+            logging.StreamHandler()
+        ])
+
+def parse_arguments():
+        parser = argparse.ArgumentParser(description='Date range excicuteable for the CARRA2 Module')
+        parser.add_argument("-st","--sday", type=str,help="Please input the start day")
+        parser.add_argument("-en","--eday", type=str,help="Please input the end day")
+        parser.add_argument("-re","--res", type=int,choices=[1000,2500,5000],default=2500,help="Please input the resolution of the output")
+        parser.add_argument("-ar","--area", type=str,default=None,help="Please input the areas you want to process")
+        parser.add_argument("-o","--output", type=str,default="tif",choices=["tif","csv","nc"],help="Please specify the out format")
+        parser.add_argument("-c","--cores", type=int,default=4,help="Please input the number of cores you want to use")
+        parser.add_argument("-se","--season", type=str,default="default",help="Please input the season or month you want to process")
+        args = parser.parse_args()
+        return args
+    
+    
+def multicarra2(date,res,area,out):
+  
+    pathfinder = carra2py.AVHRR(date,block=True)
+    data = pathfinder.proc(area=area,res=res)
+    
+    if data:    
+        if out == 'tif':
+            pathfinder.export_to_tif(output = data)
+    
+        elif out == 'nc':
+            pathfinder.export_to_nc(output = data)
+    
+        else: 
+            pathfinder.export_to_csv(output = data)
+            
+            
+if __name__ == "__main__":
+    
+    args = parse_arguments() 
+    
+    if args.season == "default":
+        months = ["05","06","07","08","09"]
+    elif (args.season).isnumeric():
+        months = [args.season]
+    else: 
+        months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+        
+        
+    
+    carra_f = os.path.abspath('..')
+    output_f = carra_f + os.sep + 'output'
+    
+    dates = pd.date_range(start=args.sday,end=args.eday).to_pydatetime().tolist()
+    dates = [d.strftime("%Y%m%d") for d in dates]
+    dates = [d for d in dates if d[4:6] in [ m for m in months]]
+    res_str = str(args.res)
+    out_str = args.output
+    
+    if os.path.exists(output_f): 
+        f_done = glob.glob(output_f + os.sep + '**' + os.sep + f'*{res_str}m_AVHRR.{out_str}',recursive=True)
+        f_done_dates = [f.split(os.sep)[-1][:8] for f in f_done]
+        dates = [d for d in dates if d not in f_done_dates]
+    
+    
+    res = [args.res for i in range(len(dates))]
+    area = [args.area for i in range(len(dates))]
+    out = [args.output for i in range(len(dates))]
+   
+    
+    logging.info("Processing for date range: " + args.sday + " to " + args.eday)
+    logging.info("Number of Days: " + str(len(dates)))
+   
+    logging.info(f"Number of Cores: {args.cores}")
+    
+    set_start_method("spawn")
+    
+    with get_context("spawn").Pool(args.cores) as p:       
+            p.starmap(multicarra2,zip(dates,res,area,out))
+    
+    logging.info("Processing Done!")
